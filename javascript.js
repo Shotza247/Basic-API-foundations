0ff14ad1c8659c2c4a9b44527817c1d6d2a689b4
Jabulani Ndlovu <jabulani.ndlovu360@gmail.com>
2025-08-12 10:19:58 +0200

    Updated the js file to create a card view and have a pop-up modal to view each items details this is triggered by the view details button. Adjusted the modals images size and responsiveness.

--- javascript.js
+++ javascript.js
@@ -1,18 +1,17 @@
 fetch('https://hplussport.com/api/products?order=price')
-.then(
-    function (response) 
-    {
-        return response.json();
-    }
-)
-.then(
-    function (data) 
-    {
-    
-     // Created a container UL for products
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    // Create a container UL for products
     var productList = document.createElement('ul');
     productList.className = 'product-list';
     document.body.appendChild(productList);
+
+    // Create modal container
+    var modal = document.createElement('div');
+    modal.className = 'modal';
+    document.body.appendChild(modal);
 
     for (let item of data) {
         // Create product item container
@@ -34,20 +33,23 @@ fetch('https://hplussport.com/api/products?order=price')
         name.className = 'product-name';
         name.innerHTML = item.name;
 
-        // Product description
-        var desc = document.createElement('div');
-        desc.className = 'product-description';
-        desc.innerHTML = item.description;
-
         // Product price
         var price = document.createElement('div');
         price.className = 'product-price';
         price.innerHTML = 'R' + item.price;
 
+        // View Details Button
+        var viewDetailsBtn = document.createElement('button');
+        viewDetailsBtn.className = 'view-details-btn';
+        viewDetailsBtn.innerHTML = 'View Details';
+        viewDetailsBtn.onclick = function() {
+            showProductDetails(item);
+        };
+
         // Assemble details
         details.appendChild(name);
-        details.appendChild(desc);
         details.appendChild(price);
+        details.appendChild(viewDetailsBtn);
 
         // Assemble product item
         productItem.appendChild(productImg);
@@ -55,5 +57,28 @@ fetch('https://hplussport.com/api/products?order=price')
 
         // Add to product list
         productList.appendChild(productItem);
+    }
+
+    // Function to show product details in modal
+    function showProductDetails(item) {
+    modal.innerHTML = `
+        <div class="modal-content">
+            <span class="close-btn" onclick="document.querySelector('.modal').style.display='none'">&times;</span>
+            <div class="modal-image-container">
+                <img src="${item.image}" alt="${item.name}">
+            </div>
+            <h2>${item.name}</h2>
+            <p class="product-description">${item.description}</p>
+            <div class="product-price">R${item.price}</div>
+        </div>
+    `;
+    modal.style.display = 'block';
+}
+
+    // Close modal when clicking outside
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
     }
 });
